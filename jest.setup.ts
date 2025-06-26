e8b1c3879bf408891d5f3bccc7aeaac9c1dabc29
Rafael Antunes <rafao.lima@hotmail.com>
2025-06-25 23:11:21 -0300

    Ajuste no teste

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom';
+
+Object.defineProperty(window, 'speechSynthesis', {
+    value: {
+        cancel: jest.fn(),
+        speak: jest.fn(),
+        pause: jest.fn(),
+        resume: jest.fn()
+    },
+    writable: true,
+});
